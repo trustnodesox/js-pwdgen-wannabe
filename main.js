59acdec1e7b5c6e3d7b01d45aa4221f44c78ffd9
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-01 01:27:33 +0100

    completo

---
+++ main.js
@@ -0,0 +1,13 @@
+//nome
+var nome = prompt ("Dimmi il tuo nome")
+console.log(nome);
+//cognome
+var cognome = prompt ("Dimmi il tuo cognome")
+console.log(cognome);
+//colore
+var colore = prompt ("Dimmi il tuo colore preferito")
+console.log(colore);
+
+
+
+document.getElementById('pass').innerHTML = nome + cognome + colore + "21";
